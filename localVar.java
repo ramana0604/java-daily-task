0fed94c805bacac68287fdb3c90efced2c7c1d59
ramana0604 <ramanaraja145@gmail.com>
2024-09-12 21:33:27 +0530

    mathamatical opration

---
+++ localVar.java
@@ -0,0 +1,23 @@
+package myfirstjavafile;
+
+
+
+public class localVar {
+	public static void main(String args[])
+	{
+//		----------Q1)simple mathematical problems
+		int a=10,b=2;
+		int c=a+b;
+		int d=a-b;
+		int e=a*b;
+		int div=a/b;
+		System.out.println("the addition value of a and b is ="+c);
+		System.out.println("the subtraction value of a and b is ="+d);
+		System.out.println("the multiplication value of a and b is ="+e);
+		System.out.println("the divition value of a and b is ="+div);
+
+		
+		
+	}
+
+}
